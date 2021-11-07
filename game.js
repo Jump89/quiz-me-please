c68e05cfc3913e0f43a6eeadbb668f295eb30300
Marcio R <Murcielago203@gmail.com>
2021-11-07 14:29:31 -0500

    javascript update

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ let availableQuestions = []
 // Add questions 
 let questions = [
     {
-        question: "what is 2 + 2?"
+        question: "what is 2 + 2?",
         choice1: "2",
         choice2: "4",
         choice3: "6",
@@ -21,7 +21,7 @@ let questions = [
         answer: 2,
     },
     {
-        question: "what is 2 + 2?"
+        question: "what is 2 + 2?",
         choice1: "2",
         choice2: "4",
         choice3: "6",
@@ -29,7 +29,7 @@ let questions = [
         answer: 2,
     },
     {
-        question: "what is 2 + 2?"
+        question: "what is 2 + 1?",
         choice1: "2",
         choice2: "4",
         choice3: "6",
@@ -37,7 +37,7 @@ let questions = [
         answer: 2,
     },
     {
-        question: "what is 2 + 2?"
+        question: "what is 2 + 2?",
         choice1: "2",
         choice2: "4",
         choice3: "6",
@@ -45,7 +45,7 @@ let questions = [
         answer: 2,
     },
     {
-        question: "what is 2 + 2?"
+        question: "what is 2 + 11?",
         choice1: "2",
         choice2: "4",
         choice3: "6",
@@ -53,7 +53,7 @@ let questions = [
         answer: 2,
     },
     {
-        question: "what is 2 + 2?"
+        question: "what is 2 + 10?",
         choice1: "2",
         choice2: "4",
         choice3: "6",
@@ -61,14 +61,14 @@ let questions = [
         answer: 2,
     },
     {
-        question: "what is 2 + 2?"
+        question: "what is 2 + 3?",
         choice1: "2",
         choice2: "4",
         choice3: "6",
         choice4: "10",
         answer: 2,
     },
-]
+];
 
 const SCORE_POINTS = 100
 const MAX_QUESTIONS = 7 //change once it has been determined how many questions we want 
@@ -78,17 +78,57 @@ const MAX_QUESTIONS = 7 //change once it has been determined how many questions 
 startGame = () => {
     questionCounter = 0
     score = 0
-    availableQuestions = [...questions]
+    availableQuestions = [...questions] // spread operator to get all values from questions
     getNewQuestion()
 
-}
-// keep track of scores and sends guest to new page 
+};
+// created new function to keep track of scores and sends guest to new page 
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem("mostRecentScore", score)
+
         return window.location.assign("/end.html")
     }
 
     questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-}
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}` // increment's questions by 1 
+    progressBarFull.getElementsByClassName.width = `${(questionCounter/MAX_QUESTIONS) * 100}%` // calculate which question user is on and percentage user is on 
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length) // calculate value of question index 
+    currentQuestion = availableQuestions[questionsIndex] // keep track of question user is on 
+    question.innerText = currentQuestion.question // which question to ask 
+
+    choices.forEach(choice => {
+        const number = choice.dataset('number') // will know what user has clicked on 
+        choice.innerText = currentQuestion['choice' + number] 
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+};
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) 
+        return
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset('number')
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
+        'incorrect'
+
+        if(classToApply === 'correct')
+            incrementScore(SCORE_POINTS)
+
+            selectedChoice.parentElement.classList.add(classToApply)
+    })
+});
+
+incrementScore = num => {
+    score +=num
+    score.Text.innerText = score
+};
+
+startGame();
